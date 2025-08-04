42e2bb3442155134f0866592d7aa495a1a064fbb
SouravM16 <souravmalviya08@gmail.com>
2025-08-04 18:54:00 +0530

    changes in useerinfo() function

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ app.get('/me', authenticateToken, logger, (req, res) => {
 
 // Start server
 app.listen(3000, () => {
-    console.log("✅ Server running at http://localhost:3000");
+    console.log("Server running at http://localhost:3000");
 });
